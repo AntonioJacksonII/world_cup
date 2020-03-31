51acdff5f5a3e8cffc722319afb4e146980fb916
Antonio Jackson <antoniojackson@gmail.com>
2020-03-30 22:40:17 -0600

    Adding players_by_position with test to Team class

--- test/team_test.rb
+++ test/team_test.rb
@@ -41,7 +41,17 @@ class TeamTest < Minitest::Test
     team.add_player(pogba)
 
     assert_equal [mbappe, pogba], team.players
+  end
 
+  def test_players_by_position
+    team = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    team.add_player(mbappe)
+    team.add_player(pogba)
+
+    assert_equal [pogba], team.players_by_position("midfielder")
+    assert_equal [], team.players_by_position("defender")
   end
 
 end
